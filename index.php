a20020809e4353a0743ad17ad5439657f231a726
Benjamin Benoit <benjaminbenoit@macbook-pro-de-benjamin.home>
2023-02-07 14:39:39 +0100

    maj php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<!DOCTYPE html>
+<?php
+
+$servername = "localhost";
+$database = "vap_factory";
+$username = "root";
+$password = "root";
+
+try
+{
+    $con = new PDO("mysql:host=$servername;$database", $username, $password);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e)
+{
+        echo "Connection Failed" .$e->getMessage();
+}
+
+$reference = $_GET["reference"] ?? null;
+$name = $_GET["name"] ?? null;
+$resume = $_GET["resume"] ?? null;
+$purchasePrice = $_GET["purchasePrice"] ?? null;
+$sellingPrice = $_GET["sellingPrice"] ?? null;
+
+$sql = "INSERT INTO 'Cigarette_eletronique' ('reference', 'nom', 'description', 'prix_achat_unitaire', 'prix_vente_unitaire') VALUES ('$reference', '$name', '$resume', '$purchasePrice', '$sellingPrice';)";
+
+?><!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -11,19 +36,19 @@
     <header class="header">
         <h1 class="header__title">VAP FACTORY</h1>
     </header>
-    <form class="newProduct" action="">
+    <form class="newProduct" action="index.php" method="GET">
         <div class="newProduct_bloc1">
-            <input class="newProduct_bloc1__ref" type="text" placeholder="Référence">
-            <input class="newProduct_bloc1__name" type="text" placeholder="Nom">
+            <input class="newProduct_bloc1__ref" type="text" placeholder="Référence" id="reference" name="reference">
+            <input class="newProduct_bloc1__name" type="text" placeholder="Nom" id="name" name="name">
         </div>
-        <input class="newProduct_bloc2__resume" type="text" placeholder="Description">
+        <input class="newProduct_bloc2__resume" type="text" placeholder="Description" id="resume" name="resume">
         <div class="newProduct_bloc3">
-            <input class="newProduct_bloc3__purchasePrice" type="text" placeholder="Prix d'achat">
-            <input class="newProduct_bloc3__sellingPrice" type="text" placeholder="Prix de vente">
+            <input class="newProduct_bloc3__purchasePrice" type="text" placeholder="Prix d'achat" id="purchasePrice" name="purchasePrice">
+            <input class="newProduct_bloc3__sellingPrice" type="text" placeholder="Prix de vente" id="sellingPrice" name="sellingPrice">
         </div>
         <div class="newProduct_bloc4">
-            <input class="newProduct_bloc4__stock" type="text" placeholder="Stock">
-            <button class="newProduct_bloc4__btnAdd" class="btnAjout">AJOUTER</button>
+            <input class="newProduct_bloc4__stock" type="text" placeholder="Stock" id="stock" name="stock">
+            <button type="submit" class="newProduct_bloc4__btnAdd" class="btnAjout">AJOUTER</button>
         </div>
     </form>
 </body>
